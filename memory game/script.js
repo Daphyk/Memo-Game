3de03768d973bf66bfd32fa132e8a137f7182f61
Daphyk <kendid07@gmail.com>
2023-04-13 23:38:10 +0300

    a

---
+++ memory game/script.js
@@ -0,0 +1,67 @@
+const cards = dosument.querySelectorAll('.memory-card');
+
+let hasFlippedCard = false;
+let lockBoard = false;
+let firstCard, secondCard;
+
+function flipCard() {
+    if (lockBoard) return;
+    if (this === firstCard) return;
+
+    this.classList.add('flip');
+
+    if (!hasFlippedCard) {
+        //first click 
+        hasFlippedCard = true;
+        firstCard = this;
+
+        return;
+    } else {
+        //first click
+        hasFlippedCard = false;
+        secondCard = this;
+    }
+    //second click
+    secondCard = this;
+
+
+    checkForMatch();
+}
+
+function checkForMatch() {
+    let isMatch = firstCard.dataset.framework === secondCard.farmework;
+
+    isMatch ? disableCards() : unflipCards();
+}
+
+function disableCards() {
+    firstCard.removeEventListener('click', flipCards);
+    secondCard.removeEventListener('click', flipCards);
+
+    resetBoard();
+}
+
+function unflipCards() {
+    lockBoards = true;
+
+    setTimeout(() => {
+        firstCard.classList.remove('flip');
+        secondCard.classList.remove('flip');
+
+        resetBoard();
+    }, 1500);
+}
+
+function resetBoard() {
+    [hasFlippedCard, lockBoard] = [false, false];
+    [firstCard, secondCard] = [null, null];
+}
+
+(function shuffle() {
+    cards.forEach(card => {
+        let randomPos = Math.floor(Math.random() * 12);
+        card.style.order = randomPos;
+    });
+})();
+
+cards.forEach(card => card.addEventListener('click', flipCard));
